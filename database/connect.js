319f366d32d02273a27c5373cd8ded23e5de6ae1
Harsh Deep <hdeep61034@gmail.com>
2023-11-23 19:07:15 +0530

    debugging server errors

--- database/connect.js
+++ database/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 function connectToDB() {
-	require("dotenv").config();
-	const mongoDBURL = process.env.MONGODB_URI;
+	// require("dotenv").config();
+	const mongoDBURL = "mongodb+srv://admin:H@rsh7017@cluster0.pkzzmbo.mongodb.net/";
 	mongoose.connect(mongoDBURL).then(console.log("Connected To MonngoDB")).catch((err) => { console.log("Mongo DB Err: ", err) });
 }
 
