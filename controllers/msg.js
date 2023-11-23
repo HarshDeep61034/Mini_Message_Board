707bf31b5eabd03f2c09acb13ae60bd282fd5b6a
Harsh Deep <hdeep61034@gmail.com>
2023-11-23 18:56:56 +0530

    changed MongoURL for cloud deployment and fixed some links

--- controllers/msg.js
+++ controllers/msg.js
@@ -3,7 +3,7 @@ async function createMsg(req, res) {
 	const { user, text } = req.body;
 	Msg.create({ user, text });
 	const allMsgs = await Msg.find({});
-	res.redirect("http://localhost:2222/");
+	res.redirect("/");
 }
 
 module.exports = { createMsg }
--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
 function connectToDB() {
-	mongoose.connect("mongodb://127.0.0.1:27017/msgDb").then(console.log("Connected To MonngoDB")).catch((err) => { console.log("Mongo DB Err: ", err) });
+	require("dotenv").config();
+	const mongoDBURL = process.env.MONGODB_URI;
+	mongoose.connect(mongoDBURL).then(console.log("Connected To MonngoDB")).catch((err) => { console.log("Mongo DB Err: ", err) });
 }
 
 module.exports = connectToDB;
